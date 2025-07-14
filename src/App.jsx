2d92b573e627c3781289993db7a06b7dd52cd34b
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2025-07-14 12:53:25 +0330

    create app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="p-20 bg-cyan-950 text-amber-200 h-screen">
+      <h1 className="text-3xl font-bold">About :</h1>
+      <p className="text-lg leading-8">
+        In this page, I will practice all important components.
+      </p>
+    </div>
+  );
+}
+
+export default App;
